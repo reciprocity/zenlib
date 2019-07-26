3ea9d3c7cdd24270975a2b409ecaee0d9e240cd9
Leonardo Ferreyra <leonardo@reciprocitylabs.com>
2019-07-26 09:58:08 -0300

    Add button component

---
+++ packages/button/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <b-button v-bind="$attrs" :class="{ loading: showLoader }" v-on="$listeners">
+    <i v-if="showLoader" class="fa fa-circle-o-notch fa-spin spinner" />
+    <span class="text">
+      <slot />
+    </span>
+  </b-button>
+</template>
+
+<script>
+	import bButton from 'bootstrap-vue/es/components/button/button';
+
+	export default {
+		name: 'Button',
+		components: { bButton },
+		props: {
+			showLoader: {
+				type: Boolean,
+				default: false,
+			},
+		},
+	};
+</script>
+
+<style lang="scss" scoped>
+	.loading {
+		position: relative;
+
+		.text {
+			visibility: hidden;
+		}
+
+		.spinner {
+			left: 50%;
+			margin: -7px 0 0 -7px;
+			position: absolute;
+			top: 50%;
+		}
+	}
+</style>
