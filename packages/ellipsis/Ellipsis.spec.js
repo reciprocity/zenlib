3f6315085d0e044e33f7b9540776002ccaffc863
pr1moz <primozz.kos@gmail.com>
2019-11-22 10:17:01 +0000

    test: add additional test

--- packages/ellipsis/Ellipsis.spec.js
+++ packages/ellipsis/Ellipsis.spec.js
@@ -6,4 +6,11 @@ describe("Ellipsis", () => {
     const wrapper = shallowMount(Ellipsis);
     expect(wrapper.exists()).toBe(true);
   });
+
+  it("should expand when More link is clicked", () => {
+    const wrapper = shallowMount(Ellipsis);
+    expect(wrapper.find(".text-ellipsis").classes("is-expanded")).toBe(false);
+    wrapper.find(".ellipsis-toggle").trigger("click");
+    expect(wrapper.find(".text-ellipsis").classes("is-expanded")).toBe(true);
+  });
 });
