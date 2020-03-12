efd8da6f78793b8cee8cfd85bacb3b6837249bc9
Santiago Bandiera <sagaban@hotmail.com>
2020-03-12 17:06:47 -0300

    feat: implement component, change styles, improve storybook

--- packages/_storybook_/stories/DaterPicker.stories.js
+++ packages/_storybook_/stories/DaterPicker.stories.js
@@ -1,4 +1,5 @@
 import { storiesOf } from "@storybook/vue";
+import { action } from "@storybook/addon-actions";
 import DatePicker from "@reciprocity/date-picker";
 import README from "../../date-picker/README.md";
 
@@ -8,7 +9,51 @@ storiesOf("DatePicker", module)
       sidebar: README
     }
   })
-  .add("default", () => ({
+  .add("basic", () => ({
+    data() {
+      return {
+        time: null
+      };
+    },
     components: { DatePicker },
-    template: "<date-picker />"
+    template:
+      '<date-picker :value="time" valueType="format" @input="handleInput"/>',
+    methods: {
+      handleInput(time) {
+        this.time = time;
+        action("input");
+      }
+    }
+  }))
+  .add("datetime", () => ({
+    data() {
+      return {
+        time: null
+      };
+    },
+    components: { DatePicker },
+    template: '<date-picker v-model="time" type="datetime"/>'
+  }))
+  .add("range", () => ({
+    data() {
+      return {
+        time: null,
+        lang: {
+          monthFormat: "MMM"
+        }
+      };
+    },
+    components: { DatePicker },
+    template: '<date-picker v-model="time" range :lang="lang">'
+  }))
+  .add("slots", () => ({
+    data() {
+      return {
+        time: null
+      };
+    },
+    components: { DatePicker },
+    template: `<date-picker v-model="time" valueType="format">
+        <template v-slot:header> <span> Custom header </span></template>
+        </date-picker>`
   }));
--- packages/date-picker/DatePicker.vue
+++ packages/date-picker/DatePicker.vue
@@ -1,13 +1,32 @@
 <template>
-  <div>
-    Datepicker
-  </div>
+  <date-picker
+    class="date-picker"
+    :lang="lang"
+    v-bind="$attrs"
+    v-on="$listeners"
+  >
+    <slot v-for="(_, name) in $slots" :slot="name" :name="name" />
+  </date-picker>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
+import DatePicker from "vue2-datepicker";
+import "./datepicker.scss";
 
-export default Vue.extend({});
+export default Vue.extend({
+  components: {
+    DatePicker
+  },
+  props: {
+    lang: {
+      type: Object,
+      default() {
+        return {
+          monthFormat: "MMMM"
+        };
+      }
+    }
+  }
+});
 </script>
-
-<style scoped></style>
