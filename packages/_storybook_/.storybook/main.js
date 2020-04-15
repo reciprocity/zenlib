a7f42635c57d6b2f090f1ac1f3b09f4ca371033c
Josefina Espinosa <joc.luz@gmail.com>
2020-04-15 14:48:32 -0300

    build: upgrade storybook

---
+++ packages/_storybook_/.storybook/main.js
@@ -0,0 +1,9 @@
+module.exports = {
+  stories: ["../**/*.stories.js"],
+  addons: [
+    "@storybook/preset-scss",
+    "@storybook/addon-actions",
+    "@storybook/addon-links",
+    "storybook-readme/register"
+  ],
+};
