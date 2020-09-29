3e8a95e832cbacc6db6fae7af857105fbe239f85
Jan Savli <jan.savli@gmail.com>
2020-09-29 11:50:20 +0200

    style: add blocks variant

--- packages/file-dropzone/FileDropzone.vue
+++ packages/file-dropzone/FileDropzone.vue
@@ -10,12 +10,12 @@
         @dragleave.prevent="dragOut"
         @dragend.prevent="dragOut"
       >
-        <span class="d-flex align-items-center">
+        <div class="content">
           <i class="fa fa-cloud-upload mr-3" />
-          <b>Drag &amp; drop files here</b>
-          <span class="mx-2">or</span>
+          <b class="mt-0">Drag &amp; drop files here</b>
+          <span class="mx-2 mb-2 mt-1">or</span>
           <div class="fake-button">Browse files</div>
-        </span>
+        </div>
         <input
           ref="fileInput"
           :multiple="multiple"
@@ -88,11 +88,25 @@ export default {
 @import "@reciprocity/styles/scss/colors";
 
 .file-upload-dropzone {
+  .content {
+    display: flex;
+    align-items: center;
+  }
+
+  &.blocks .content {
+    flex-direction: column;
+
+    > * {
+      margin-left: 0 !important;
+      margin-right: 0 !important;
+    }
+  }
+
   .dropzone-box {
     display: flex;
     justify-content: center;
     align-items: center;
-    height: 150px;
+    height: 170px;
     border: 1px dashed $color-ghost;
     padding: 0.5rem;
     text-align: center;
